b77b679afb5aefb80c3532a19561fc74888bd24e
VS <hi.vaibhavsingh@gmail.com>
2024-07-03 23:18:41 +0530

    cleaningup

---
+++ google-chrome/Profile 2/Extensions/kbfnbcaeplbcioakkpcpgfkobkghlhen/14.1185.0_0/src/js/newToneDetectedPopup.common.chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunk=self.webpackChunk||[]).push([[9704],{88008:(e,t,a)=>{a.r(t),a.d(t,{NewToneDetectedPopup:()=>p});var r=a(49231),o=a(30165);const p=()=>r.createElement("div",{className:o.wrapper,"data-grammarly-part":"newToneDetectedPopup"},"New tone detected! Hover over the Grammarly widget and click the emoji to check your tone.")},30165:e=>{e.exports={wrapper:"BFyE2"}}}]);
